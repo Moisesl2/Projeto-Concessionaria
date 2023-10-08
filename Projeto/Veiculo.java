03fecc5ad35d084b64941a68522eac948234db85
Verinha <143955529+veragad@users.noreply.github.com>
2023-10-08 15:14:28 -0300

    Update Veiculo.java

--- Projeto/Veiculo.java
+++ Projeto/Veiculo.java
@@ -1,45 +1,29 @@
-package concessionaria;
-import java.util.ArrayList; 
-import java.util.Iterator;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Veiculo {
-    private int codigo;
-    private String marca;
+	private String marca;
     private String modelo;
-    private int anoFabricacao;
+    private int anoDeFabricacao;
     private String cor;
     private String estadoDoVeiculo;
     private int quantidade;
 
-    // Construtor
-    public Veiculo(int codigo, String marca, String modelo, int anoFabricacao, String cor, String estadoDoVeiculo, int quantidade) {
-        this.codigo= codigo;
-    	this.marca = marca;
+    public Veiculo(String marca, String modelo, int anoDeFabricacao, String cor, String estadoDoVeiculo, int quantidade) {
+        this.marca = marca;
         this.modelo = modelo;
-        this.anoFabricacao = anoFabricacao;
+        this.anoDeFabricacao = anoDeFabricacao;
         this.cor = cor;
         this.estadoDoVeiculo = estadoDoVeiculo;
         this.quantidade = quantidade;
     }
+
     
-    //Getters e Setters
-
-	public int getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	
-	public String getMarca() {
+    public String getMarca() {
 		return marca;
 	}
-	
+
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
@@ -55,13 +39,13 @@ public class Veiculo {
 	}
 
 
-	public int getAnoFabricacao() {
-		return anoFabricacao;
+	public int getAnoDeFabricacao() {
+		return anoDeFabricacao;
 	}
 
 
-	public void setAnoFabricacao(int anoFabricacao) {
-		this.anoFabricacao = anoFabricacao;
+	public void setAnoDeFabricacao(int anoDeFabricacao) {
+		this.anoDeFabricacao = anoDeFabricacao;
 	}
 
 
@@ -94,151 +78,166 @@ public class Veiculo {
 		this.quantidade = quantidade;
 	}
 
+   //Criar veículo
+	public static Veiculo criarVeiculoEmArquivo(String marca, String modelo, int anoDeFabricacao, String cor, String estadoDoVeiculo, int quantidade, String nomeArquivo) {
+        Veiculo veiculo = new Veiculo(marca, modelo, anoDeFabricacao, cor, estadoDoVeiculo, quantidade);
+        List<Veiculo> veiculos = lerVeiculosEmArquivo(nomeArquivo);
+        veiculos.add(veiculo);
+        salvarVeiculosEmArquivo(veiculos, nomeArquivo);
+        return veiculo;
+    }
 
-	// Método para cadastrar um novo veículo
-	 public static void cadastrarVeiculo(List<Veiculo> listaVeiculos, int codigo, String marca, String modelo, int anoFabricacao, String cor, String estadoDoVeiculo, int quantidade) {
-	        Veiculo novoVeiculo = new Veiculo(codigo, marca, modelo, anoFabricacao, cor, estadoDoVeiculo, quantidade);
-	        listaVeiculos.add(novoVeiculo);
-	 }
+    //Atualizar Veículo
+    public void atualizarVeiculoEmArquivo(String nomeArquivo) {
+        List<Veiculo> veiculos = lerVeiculosEmArquivo(nomeArquivo);
+        for (int i = 0; i < veiculos.size(); i++) {
+            if (veiculos.get(i).marca.equals(this.marca) && veiculos.get(i).modelo.equals(this.modelo)) {
+                veiculos.set(i, this);
+                salvarVeiculosEmArquivo(veiculos, nomeArquivo);
+                break;
+            }
+        }
+    }
+    //Excluir Veiculo
+    public static void excluirVeiculoEmArquivo(String marca, String modelo, String nomeArquivo) {
+        List<Veiculo> veiculos = lerVeiculosEmArquivo(nomeArquivo);
+        for (int i = 0; i < veiculos.size(); i++) {
+            if (veiculos.get(i).marca.equals(marca) && veiculos.get(i).modelo.equals(modelo)) {
+                veiculos.remove(i);
+                salvarVeiculosEmArquivo(veiculos, nomeArquivo);
+                break;
+            }
+        }
+    }
+    //Listar Veiculo igual está no arquivo 
+    public static void listarVeiculosEmArquivo(String nomeArquivo) {
+        List<Veiculo> veiculos = lerVeiculosEmArquivo(nomeArquivo);
+        for (Veiculo veiculo : veiculos) {
+            veiculo.lerVeiculo();
+            System.out.println("============================");
+        }
+    }
+    //Atualizar estoque
+    public void atualizarEstoqueEmArquivo(int quantidade, String nomeArquivo) {
+        this.quantidade += quantidade;
+        atualizarVeiculoEmArquivo(nomeArquivo);
+    }
 
-       // Método para editar  veículo
-	 public void editarVeiculo(int codigo, String novaMarca, String novoModelo, int novoAnoFabricacao, String novaCor, String novoEstadoDoVeiculo, int novaQuantidade) {
-	        this.codigo = codigo;
-		    this.marca = novaMarca;
-	        this.modelo = novoModelo;
-	        this.anoFabricacao = novoAnoFabricacao;
-	        this.cor = novaCor;
-	        this.estadoDoVeiculo = novoEstadoDoVeiculo;
-	        this.quantidade = novaQuantidade;
-	 }
+    public void lerVeiculo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano de Fabricação: " + anoDeFabricacao);
+        System.out.println("Cor: " + cor);
+        System.out.println("Estado do Veículo: " + estadoDoVeiculo);
+        System.out.println("Quantidade em Estoque: " + quantidade);
+    }
+    //Ler em arquivo
+    public static List<Veiculo> lerVeiculosEmArquivo(String nomeArquivo) {
+        List<Veiculo> veiculos = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(nomeArquivo); // Cria um leitor de arquivo
+            BufferedReader bufferedReader = new BufferedReader(fileReader); // Cria um buffer de leitura
 
-        // Método para ver os dados de um veículo
-	 public void verDadosVeiculo() {
-		    System.out.println("Detalhes do Veículo:");
-		    System.out.println("+-----------------------------------------------------------------------+");
-		    System.out.printf("| %-15s | %-15s | %-8s |%n", "Atributo", "Valor", " ");
-		    System.out.println("+-----------------------------------------------------------------------+");
-		    System.out.printf("| %-15s | %-15d | %-8s |%n", "Codigo", this.codigo, " ");
-		    System.out.printf("| %-15s | %-15s | %-8s |%n", "Marca", this.marca, " ");
-		    System.out.printf("| %-15s | %-15s | %-8s |%n", "Modelo", this.modelo, " ");
-		    System.out.printf("| %-15s | %-15d | %-8s |%n", "Ano de Fabricação", this.anoFabricacao,   " ");
-		    System.out.printf("| %-15s | %-15s | %-8s |%n", "Cor", this.cor, " ");
-		    System.out.printf("| %-15s | %-15s | %-8s |%n", "Estado do Veículo", this.estadoDoVeiculo, " ");
-		    System.out.printf("| %-15s | %-15d | %-8s |%n", "Quantidade", this.quantidade, " ");
-		    System.out.println("+-----------------------------------------------------------------------+");
+            String marca = "";
+            String modelo = "";
+            int anoDeFabricacao = 0;
+            String cor = "";
+            String estadoDoVeiculo = "";
+            int quantidade = 0;
+
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) { // Enquanto houver linhas para ler
+                if (linha.startsWith("Marca: ")) {
+                    marca = linha.substring("Marca: ".length()).trim();
+                } else if (linha.startsWith("Modelo: ")) {
+                    modelo = linha.substring("Modelo: ".length()).trim();
+                } else if (linha.startsWith("Ano de Fabricação: ")) {
+                    anoDeFabricacao = Integer.parseInt(linha.substring("Ano de Fabricação: ".length()).trim());
+                } else if (linha.startsWith("Cor: ")) {
+                    cor = linha.substring("Cor: ".length()).trim();
+                } else if (linha.startsWith("Estado do Veículo: ")) {
+                    estadoDoVeiculo = linha.substring("Estado do Veículo: ".length()).trim();
+                } else if (linha.startsWith("Quantidade em Estoque: ")) {
+                    quantidade = Integer.parseInt(linha.substring("Quantidade em Estoque: ".length()).trim());
+
+                    // Crie um novo veículo com os dados lidos da linha e adicione à lista
+                    Veiculo veiculo = new Veiculo(marca, modelo, anoDeFabricacao, cor, estadoDoVeiculo, quantidade);
+                    veiculos.add(veiculo);
+
+                    // Limpe as variáveis para o próximo veículo
+                    marca = "";
+                    modelo = "";
+                    anoDeFabricacao = 0;
+                    cor = "";
+                    estadoDoVeiculo = "";
+                    quantidade = 0;
+                }
+            }
+
+            bufferedReader.close(); // Fecha o buffer de leitura
+        } catch (IOException e) { // Trata exceções de entrada e saída
+            System.out.println("Ocorreu um erro ao carregar os veículos do arquivo: " + nomeArquivo + "\n");
+            e.printStackTrace();
+        } catch (NumberFormatException e) { // Trata exceções de conversão de número
+            System.out.println("Erro ao converter número.");
+            e.printStackTrace();
         }
 
-         // Método para listar  veículos
-	 public static void listarVeiculos(List<Veiculo> listaVeiculos) {
-	        System.out.println("+----------------------------------------------------------------------------------------+");
-	        System.out.printf(" |%-15s | %-15s | %-15s | %-15s | %-15s | %-20s | %-15s |%n", "Codigo", "Marca", "Modelo", "Ano de Fabricação", "Cor", "Estado do Veículo", "Quantidade");
-	        System.out.println("+----------------------------------------------------------------------------------------+");
+        return veiculos;
+    }
 
-	        for (Veiculo veiculo : listaVeiculos) {
-	            System.out.printf("|%-15d | %-15s | %-15s | %-15d | %-15s | %-20s | %-15d |%n",
-	                   veiculo.getCodigo(), veiculo.getMarca(), veiculo.getModelo(), veiculo.getAnoFabricacao(),
-	                    veiculo.getCor(), veiculo.getEstadoDoVeiculo(), veiculo.getQuantidade());
-	        }
-	        System.out.println("+----------------------------------------------------------------------------------------+");
-	  }
-	 
-	// Método para excluir um veículo com base no código, marca e modelo
-	 public static void excluirVeiculo(List<Veiculo> veiculos, int codigo, String marca, String modelo) {
-	        Iterator<Veiculo> iterator = veiculos.iterator();
-	        while (iterator.hasNext()) {
-	            Veiculo veiculo = iterator.next();
-	            if (veiculo.getCodigo() == codigo && veiculo.getMarca().equals(marca) && veiculo.getModelo().equals(modelo)) {
-	                iterator.remove();
-	                System.out.println("Veículo removido com sucesso.");
-	                return;
-	            }
-	        }
-	        System.out.println("Veículo não encontrado com o código, marca e modelo especificados.");
-	    } 
-	 // Método para salvar os dados dos veículos em um arquivo
-	    public static void salvarEmArquivoP(String nomeArquivo, List<Veiculo> veiculos) {
-	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
-	            for (Veiculo veiculo : veiculos) {
-	                writer.write("Marca: " + veiculo.getMarca() + "\n");
-	                writer.write("Modelo: " + veiculo.getModelo() + "\n");
-	                writer.write("Ano de Fabricação: " + veiculo.getAnoFabricacao() + "\n");
-	                writer.write("Cor: " + veiculo.getCor() + "\n");
-	                writer.write("Estado do Veículo: " + veiculo.getEstadoDoVeiculo() + "\n");
-	                writer.write("Quantidade: " + veiculo.getQuantidade() + "\n");
-	                writer.write("\n"); // Linha em branco para separar os registros
-	            }
-	            System.out.println("Dados dos veículos salvos com sucesso no arquivo: " + nomeArquivo);
-	        } catch (IOException e) {
-	            System.err.println("Erro ao salvar os dados dos veículos no arquivo: " + e.getMessage());
-	        }
-	    }
-	    
-	    // Método para ler os dados dos veículos de um arquivo
-	    public static List<Veiculo> lerArquivoV(String nomeArquivo) {
-	        List<Veiculo> veiculos = new ArrayList<>();
+    //Salvar em arquivo
+    public static void salvarVeiculosEmArquivo(List<Veiculo> veiculos, String nomeArquivo) {
+        try {
+            FileWriter fileWriter = new FileWriter(nomeArquivo); // Cria um escritor de arquivo
+            PrintWriter printWriter = new PrintWriter(fileWriter); // Cria um buffer de escrita
 
-	        try (BufferedReader reader = new BufferedReader(new FileReader(nomeArquivo))) {
-	            String linha;
-	            Veiculo veiculo = null;
+            for (Veiculo veiculo : veiculos) {
+                printWriter.println("Marca: " + veiculo.getMarca());
+                printWriter.println("Modelo: " + veiculo.getModelo());
+                printWriter.println("Ano de Fabricação: " + veiculo.getAnoDeFabricacao());
+                printWriter.println("Cor: " + veiculo.getCor());
+                printWriter.println("Estado do Veículo: " + veiculo.getEstadoDoVeiculo());
+                printWriter.println("Quantidade em Estoque: " + veiculo.getQuantidade());
+                printWriter.println(); // Adiciona uma linha em branco para separar os registros
+            }
+            printWriter.close(); // Fecha o buffer de escrita
+        } catch (IOException e) { // Trata exceções de entrada e saída
+            System.out.println("Ocorreu um erro ao salvar os veículos no arquivo: " + nomeArquivo + "\n");
+            e.printStackTrace();
+        }
+    }
+    
+    ////////////////////////////////////////
+    public static void imprimirVeiculosEmTabela(List<Veiculo> veiculos) {
+        // Imprime o cabeçalho da tabela
+        System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+----------------------+");
+        System.out.println("|        Marca         |      Modelo          |  Ano de Fabricação   |         Cor          |   Estado do Veículo  |      Quantidade      |");
+        System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+----------------------+");
 
-	            while ((linha = reader.readLine()) != null) {
-	                if (linha.trim().isEmpty()) {
-	                    // Linha em branco indica o final de um registro, então adicionamos o veículo à lista
-	                    if (veiculo != null) {
-	                        veiculos.add(veiculo);
-	                        veiculo = null; // Reinicializa o veículo
-	                    }
-	                } else {
-	                    String[] partes = linha.split(": ");
-	                    if (partes.length == 2) {
-	                        String atributo = partes[0];
-	                        String valor = partes[1];
+        // Itera pelos veículos e imprime cada um deles na tabela
+        for (Veiculo veiculo : veiculos) {
+            System.out.printf("| %-20s | %-20s | %-20d | %-20s | %-20s | %-20d |\n", veiculo.getMarca(), veiculo.getModelo(), veiculo.getAnoDeFabricacao(), veiculo.getCor(), veiculo.getEstadoDoVeiculo(), veiculo.getQuantidade());
+        }
 
-	                        switch (atributo) {
-	                            case "Marca":
-	                                veiculo = new Veiculo();
-	                                veiculo.setMarca(valor);
-	                                break;
-	                            case "Modelo":
-	                                veiculo.setModelo(valor);
-	                                break;
-	                            case "Ano de Fabricação":
-	                                veiculo.setAnoFabricacao(Integer.parseInt(valor));
-	                                break;
-	                            case "Cor":
-	                                veiculo.setCor(valor);
-	                                break;
-	                            case "Estado do Veículo":
-	                                veiculo.setEstadoDoVeiculo(valor);
-	                                break;
-	                            case "Quantidade":
-	                                veiculo.setQuantidade(Integer.parseInt(valor));
-	                                break;
-	                            default:
-	                                // Ignora atributos desconhecidos
-	                                break;
-	                        }
-	                    }
-	                }
-	            }
+        // Imprime o rodapé da tabela
+        System.out.println("+----------------------+----------------------+----------------------+----------------------+----------------------+----------------------+");
+        System.out.println();
+    }
 
-	            // Adiciona o último veículo se houver um no final do arquivo
-	            if (veiculo != null) {
-	                veiculos.add(veiculo);
-	            }
 
-	        } catch (IOException e) {
-	            System.err.println("Erro ao ler o arquivo de veículos: " + e.getMessage());
-	        }
-
-	        return veiculos;
-	    }
-
-	    
-	 @Override
-	 public String toString() {
-		return "Veiculo [codigo=" + codigo + ", marca=" + marca + ", modelo=" + modelo + ", anoFabricacao=" + anoFabricacao
-				+ ", cor=" + cor + ", estadoDoVeiculo=" + estadoDoVeiculo + ", quantidade=" + quantidade + "]";
+	@Override
+	public String toString() {
+		return "Veiculo [marca=" + marca + ", modelo=" + modelo + ", anoDeFabricacao=" + anoDeFabricacao + ", cor="
+				+ cor + ", estadoDoVeiculo=" + estadoDoVeiculo + ", quantidade=" + quantidade + "]";
 	}
-    	
+    
 }
+	
+	    
+    
+	
+
+	
+		
+
